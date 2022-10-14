711669a9232cac9d4f79ca00a835dcb0c7e38352
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-14 11:27:01 -0700

    index

--- Assets/Script/index.js
+++ Assets/Script/index.js
@@ -2,19 +2,34 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext("2d");
-
+var eventKey;
+var charPX = 500;
+var charPY = 500;
 document.addEventListener("keypress", e => {
-    c.beginPath();
-    c.fillStyle = "black";
-    c.font = "italic bold 40px arial,serif"
-    c.fillText(event.key, 200, 200);
-    c.fill();
+    debug();
+    eventKey = event.key;
+    if (eventKey == "a"){
+        charPX -= 5;
+    }
+    if (eventKey == "d"){
+        charPX += 5;
+    }
 });
 
 function draw() {
     requestAnimationFrame(draw);
     //c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
+    c.beginPath();
+    c.fillStyle = "black";
+    c.fillRect(charPX, charPY, 20, 20);
+    c.fill();
 }
 
 draw();
+
+function debug() {
+    c.beginPath();
+    c.font = "italic bold 40px arial,serif"
+    c.fillText(eventKey, 200, 200);
+    c.fill();
+}
