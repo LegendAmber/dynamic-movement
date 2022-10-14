6d9ce8da14849cd7921be4e8d841bc3db2e98935
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-14 09:54:25 -0700

    js

---
+++ Assets/Script/index.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var c = canvas.getContext("2d");
+
+function draw() {
+    requestAnimationFrame(draw);
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    c.beginPath();
+    c.fillStyle = "black";
+    c.fillRect(200, 200, 20, 20);
+    c.fill();
+}
