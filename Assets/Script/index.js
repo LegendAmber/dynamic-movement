769a80122c629522e4b36e095a3ef391f68c88e3
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-14 09:57:26 -0700

    hs

--- Assets/Script/index.js
+++ Assets/Script/index.js
@@ -1,7 +1,7 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var c = canvas.getContext("2d");
+var c = canvas.getContext('2d');
 
 function draw() {
     requestAnimationFrame(draw);
